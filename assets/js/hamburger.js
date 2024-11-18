39028355fd1171677ee2fbc36f83ea34defd717f
bambi0256 <bambi0256@gmail.com>
2024-11-18 23:50:52 +0900

    Update Hamburger Menu

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     const hamburgerMenu = document.querySelector(".hamburger-menu");
-    const dropdownMenu = document.querySelector(".dropdown-menu");
+    const overlayMenu = document.querySelector(".overlay-menu");
+    const overlayClose = document.querySelector(".overlay-close");
 
+    // Open Overlay Menu
     hamburgerMenu.addEventListener("click", () => {
-        dropdownMenu.classList.toggle("active");
+        overlayMenu.classList.add("active");
+        document.body.style.overflow = "hidden"; // Lock scrolling
+    });
+
+    // Close Overlay Menu
+    overlayClose.addEventListener("click", () => {
+        overlayMenu.classList.remove("active");
+        document.body.style.overflow = ""; // Unlock scrolling
     });
 });
