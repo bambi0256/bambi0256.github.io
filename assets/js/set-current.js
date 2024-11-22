5466e370bbd09b2d7623b7481cb8f07d8ce32bab
bambi0256 <bambi0256@gmail.com>
2024-11-22 22:22:54 +0900

    Divide Stylesheet

--- assets/js/set-current.js
+++ assets/js/set-current.js
@@ -2,6 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const navLinks = document.querySelectorAll(".nav-list a");
     const currentPath = window.location.pathname;
 
+    // Remove existing 'current-page' class from all nav items
+    navLinks.forEach((link) => {
+        link.parentElement.classList.remove("current-page");
+    });
+
+    // Add 'current-page' class to the matching nav item
     navLinks.forEach((link) => {
         const linkPath = new URL(link.href).pathname;
         if (currentPath.startsWith(linkPath)) {
