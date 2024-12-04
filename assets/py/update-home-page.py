5daaf0be3e0bf41a40f36133011319ae454eff01
bambi0256 <bambi0256@gmail.com>
2024-12-04 23:04:18 +0900

    About Page Updater

--- assets/py/update-home-page.py
+++ assets/py/update-home-page.py
@@ -1,18 +1,26 @@
 import os
 from jinja2 import Environment, FileSystemLoader
 
-def update_home_page():
+def update_page(output_dir, template_file):
     # 1. Jinja2 환경 설정
-    env = Environment(loader=FileSystemLoader('./'))
-    template = env.get_template('temp-index.html')
+    env = Environment(loader=FileSystemLoader(searchpath="./"))
+    template = env.get_template(template_file)
     
     # 2. 템플릿 렌더링
     output = template.render()
     
     # 3. 파일 생성
-    output_path = './index.html'
+    output_path = os.path.join(output_dir, "index.html")
     with open(output_path, 'w') as f:
         f.write(output)
 
 if __name__ == "__main__":
-    update_home_page()
+    update_page(
+        output_dir="./",
+        template_file="./temp-index.html"
+    )
+    update_page(
+        output_dir="./about",
+        template_file="./temp-about.html"
+    )
+    
