de72831d56d7ec29159083342d47130fc2dd4edb
bambi0256 <bambi0256@gmail.com>
2024-12-24 23:29:42 +0900

    Slug Update

--- assets/js/pagination-project.js
+++ assets/js/pagination-project.js
@@ -46,7 +46,7 @@ document.addEventListener("DOMContentLoaded", function () {
                         postCard.appendChild(cardText);
 
                         // Add click event listener
-                        const slug = convertToSlug(post.title);
+                        const slug = convertToSlug(post.slug);
                         postCard.addEventListener("click", () => {
                             window.location.href = `/project/${slug}`;
                         });
